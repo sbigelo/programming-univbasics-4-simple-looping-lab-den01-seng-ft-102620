13b468db656184fe3fcc1a85ffe2b7b1ea062ea6
Sean Bigelow <sean.bigelow1@gmail.com>
2020-08-31 19:37:28 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,16 @@
-# Write your methods here
+ def loop_message_five_times(message)
+5.times{puts message}
+end
+
+def loop_message_n_times(message, number)
+  number.times{puts message}
+end
+ 
+def output_array(array)
+  counter = 0 
+  while counter < array.length do
+    puts array[counter]
+    counter += 1
+  end
+end
+
